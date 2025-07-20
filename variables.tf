a3311ccb61a0b78c3af54528c33cb666d94d1204
eswar21-sudo <eswarkumar5475@gmail.com>
2025-07-20 10:51:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "instance_type" {
   type        = string
 }
 
-variable "subnet_id" {
+variable "public_subnet_id" {
   description = "Subnet ID where the EC2 instance will be launched"
   type        = string
 }
